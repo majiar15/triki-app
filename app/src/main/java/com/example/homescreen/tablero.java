0500857f2d318a55f6f239e7d16eceb10c3095b5
Martin Jimenez <majiar16@gmail.com>
2021-03-07 15:08:24 -0500

    add turnos

--- app/src/main/java/com/example/homescreen/tablero.java
+++ app/src/main/java/com/example/homescreen/tablero.java
@@ -18,14 +18,59 @@ public class tablero extends AppCompatActivity {
     }
 
     public void handleBtn0 (View view){
-        ImageView img0 = (ImageView)findViewById(R.id.casilla0);
-        if(img0.getTag() != "circle" || img0.getTag() != "cross" ){
+        ImageView img = (ImageView)findViewById(R.id.casilla0);
+        process(img);
+        turn++;
+    }
+    public void handleBtn1 (View view){
+        ImageView img = (ImageView)findViewById(R.id.casilla1);
+        process(img);
+        turn++;
+    }
+    public void handleBtn2 (View view){
+        ImageView img = (ImageView)findViewById(R.id.casilla2);
+        process(img);
+        turn++;
+    }
+    public void handleBtn3 (View view){
+        ImageView img = (ImageView)findViewById(R.id.casilla3);
+        process(img);
+        turn++;
+    }
+    public void handleBtn4 (View view){
+        ImageView img = (ImageView)findViewById(R.id.casilla4);
+        process(img);
+        turn++;
+    }
+    public void handleBtn5 (View view){
+        ImageView img = (ImageView)findViewById(R.id.casilla5);
+        process(img);
+        turn++;
+    }
+    public void handleBtn6 (View view){
+        ImageView img = (ImageView)findViewById(R.id.casilla6);
+        process(img);
+        turn++;
+    }
+    public void handleBtn7 (View view){
+        ImageView img = (ImageView)findViewById(R.id.casilla7);
+        process(img);
+        turn++;
+    }
+    public void handleBtn8 (View view){
+        ImageView img = (ImageView)findViewById(R.id.casilla8);
+        process(img);
+        turn++;
+    }
+
+    public void process(ImageView img){
+        if(img.getTag() != "circle" || img.getTag() != "cross" ){
             if(turn %2 == 0){
-                img0.setImageResource(R.drawable.x);
-                img0.setTag("cross");
+                img.setImageResource(R.drawable.x);
+                img.setTag("cross");
             }else{
-                img0.setImageResource(R.drawable.circle);
-                img0.setTag("circle");
+                img.setImageResource(R.drawable.circle);
+                img.setTag("circle");
             }
         }
     }
