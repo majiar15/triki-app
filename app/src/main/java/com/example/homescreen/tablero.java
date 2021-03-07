a8fe911f7664bf3eb479fb11f32f6a02ca77537e
Daniel <danielsalinas70sa7@gmail.com>
2021-03-07 18:39:44 -0500

    Alerta de winner realizada

--- app/src/main/java/com/example/homescreen/tablero.java
+++ app/src/main/java/com/example/homescreen/tablero.java
@@ -2,20 +2,66 @@ package com.example.homescreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class tablero extends AppCompatActivity {
     public char turn = 0;
+
+
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tablero);
+        dialog = new Dialog(this);
+        Button btnWind =findViewById(R.id.ganador);
+        btnWind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWinDialog();
+            }
+
+
+        });
+
+
+
 
     }
+
+    private void openWinDialog() {
+        dialog.setContentView(R.layout.win_layout_dialog);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        ImageView imageViewClose= dialog.findViewById(R.id.imageViewClose);
+        Button btnSalir= dialog.findViewById(R.id.btnSalir);
+        imageViewClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                Toast.makeText(tablero.this, "Dialog close",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                Toast.makeText(tablero.this, "Button salir",Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialog.show();
+
+    }
+    
 
     public void handleBtn0 (View view){
         ImageView img = (ImageView)findViewById(R.id.casilla0);
