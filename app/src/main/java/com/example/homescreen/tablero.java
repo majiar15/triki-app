b47ce2ef9f9887a0dbd60e20c48644141fd451b0
Martin Jimenez <majiar16@gmail.com>
2021-03-07 14:44:53 -0500

    cross circle

--- app/src/main/java/com/example/homescreen/tablero.java
+++ app/src/main/java/com/example/homescreen/tablero.java
@@ -3,15 +3,31 @@ package com.example.homescreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class tablero extends AppCompatActivity {
+    public char turn = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tablero);
+
     }
-    public void Btn0(){
-        
+
+    public void handleBtn0 (View view){
+        ImageView img0 = (ImageView)findViewById(R.id.casilla0);
+        if(img0.getTag() != "circle" || img0.getTag() != "cross" ){
+            if(turn %2 == 0){
+                img0.setImageResource(R.drawable.x);
+                img0.setTag("cross");
+            }else{
+                img0.setImageResource(R.drawable.circle);
+                img0.setTag("circle");
+            }
+        }
     }
+
 }
