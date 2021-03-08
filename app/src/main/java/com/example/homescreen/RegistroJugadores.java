32109a5624246fe03da524652735dc8a5639af0d
Daniel <danielsalinas70sa7@gmail.com>
2021-03-07 20:52:49 -0500

    registro de jugadores implementado

--- app/src/main/java/com/example/homescreen/RegistroJugadores.java
+++ app/src/main/java/com/example/homescreen/RegistroJugadores.java
@@ -2,27 +2,24 @@ package com.example.homescreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class RegistroJugadores extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btn = (Button) findViewById(R.id.start);
-        btn.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_registro_jugadores);
+        Button btnJugar = (Button) findViewById(R.id.Jugar);
+        btnJugar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), RegistroJugadores.class);
+                Intent intent = new Intent(v.getContext(), tablero.class);
                 startActivityForResult(intent, 0);
             }
         });
     }
-
-
 }
